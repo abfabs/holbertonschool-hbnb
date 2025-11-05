ef610fe894ab91dfc0c78cec3db876fa39e40c29
abfabs <albaeftimi@gmail.com>
2025-11-05 20:35:02 +0100

    Initial Data Timestamps in the DataBase

--- app/database/initial_data.sql
+++ app/database/initial_data.sql
@@ -1,7 +1,8 @@
 -- HBnB Initial Data Population Script
 
+
 -- Insert Initial Amenities with UUID4 values
-INSERT INTO amenity (id, name) VALUES
-('550e8400-e29b-41d4-a716-446655440001', 'WiFi'),
-('550e8400-e29b-41d4-a716-446655440002', 'Swimming Pool'),
-('550e8400-e29b-41d4-a716-446655440003', 'Air Conditioning');
+INSERT INTO amenity (id, name, created_at, updated_at) VALUES
+('550e8400-e29b-41d4-a716-446655440001', 'WiFi', NOW(), NOW()),
+('550e8400-e29b-41d4-a716-446655440002', 'Swimming Pool', NOW(), NOW()),
+('550e8400-e29b-41d4-a716-446655440003', 'Air Conditioning', NOW(), NOW());
